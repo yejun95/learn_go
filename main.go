79d0c693b83492e97ff1d80afe0096bf044324ab
jun <xodlf100@gmail.com>
2025-04-29 00:00:09 +0900

    Functions part Two - defer 함수 -2

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 
 func lenAndUpper(name string) (int, string) {
 	defer fmt.Println("function이 끝났을 때 실행")
+	fmt.Println("------------------")
+	defer fmt.Println("111111")
+	fmt.Println(";;;;;;;;;;;;;;;;;;;")
+	defer fmt.Println("2222222222") // 제일 마지막에 있는 defer 부터 function이 끝나고 실행된다.
 	return len(name), strings.ToUpper(name)
 }
 
