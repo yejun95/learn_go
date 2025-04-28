43b61eb2893a805801ef2420780257a2b81f9bfe
jun <xodlf100@gmail.com>
2025-04-29 01:25:13 +0900

    Arrays and Slices

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a // b에 a 메모리 주소를 할당
-	*b = 20 // pointer를 통해 b값을 바꾸면 a 값도 바뀜 -> 같은 메모리 주소를 보고있기 때문
-
-	fmt.Println(a, *b)
+	names := []string{"aa", "bb", "cc"}
+	names = append(names, "dd")
+	fmt.Println(names)
 }
