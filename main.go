fbdddfbc942eea416716aa4060ae80845be1b016
jun <xodlf100@gmail.com>
2025-04-29 01:04:32 +0900

    Pointers

--- main.go
+++ main.go
@@ -2,21 +2,10 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 18:
-		return true
-	case 10:
-		return false
-	case 20:
-		return true
-	case 50:
-		return true
-	}
-
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := &a // b에 a 메모리 주소를 할당
+	*b = 20 // pointer를 통해 b값을 바꾸면 a 값도 바뀜 -> 같은 메모리 주소를 보고있기 때문
+
+	fmt.Println(a, *b)
 }
