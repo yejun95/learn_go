ed200c616ccce2e23a6fe646b8e7c4d8e6271cce
jun <xodlf100@gmail.com>
2025-04-29 23:48:47 +0900

    feat constructor를 사용한 제한적 객체 생성

---
+++ accounts/accounts.go
@@ -0,0 +1,15 @@
+package accounts
+
+// Account struct
+type Account struct {
+	owner   string
+	balance int
+}
+
+// NewAccount creates Account
+// 객체를 복사하지 않고 원본 객체를 반환
+// main에서 원본 객체에 접근하지 못하게 하면서 값을 설정 가능하게함 -> constructor
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
-import "fmt"
-
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+import (
+	"fmt"
+	"learn_go/accounts"
+)
 
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	nico := person{"test", 18, favFood}
-	nico2 := person{
-		name:    "nico",
-		age:     18,
-		favFood: favFood,
-	}
-	fmt.Println(nico)
-	fmt.Println(nico2)
+	account := accounts.NewAccount("yejun")
+	fmt.Println(account)
 }
