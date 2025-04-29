e46694808801b7305481f956af4169a2b19ac74f
jun <xodlf100@gmail.com>
2025-04-30 01:00:08 +0900

    feat Add Method

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"fmt"
 	"learn_go/mydict"
+	"log"
 )
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(definition)
+	//definition, err := dictionary.Search("first")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(definition)
 
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		log.Println(err)
+	}
+	hello, err := dictionary.Search(word)
+	fmt.Println(hello)
+
+	// 중복 검사가 잘 되는지 확인
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		log.Println(err2)
+	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,6 +6,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not found")
+var errWordExists = errors.New("That word already exists")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +15,15 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word string, def string) error {
+	_, err := d.Search(word)
+	if err == errNotFound {
+		d[word] = def
+	} else if err == nil {
+		return errWordExists
+	}
+	return nil
 }
