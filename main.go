bd64d72a39be2ad8ed484f731570a8140221b016
jun <xodlf100@gmail.com>
2025-05-02 22:56:43 +0900

    feat saramin scrapper : 카드 추출하여 배열로 완성

--- main.go
+++ main.go
@@ -1,52 +1,98 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
-type resultRequest struct {
-	url    string
-	status string
+type extractedJob struct {
+	id          string
+	badge       string
+	title       string
+	corporation string
 }
 
-var errRequestFailed = errors.New("Request failed")
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=backend"
+var baseDetailURL string = "https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no"
 
 func main() {
-	results := make(map[string]string)
-	c := make(chan resultRequest)
+	var jobs []extractedJob
+	totalPages := getPages()
+	fmt.Println("totalPages :", totalPages)
 
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.naverr.com/",
+	for i := 1; i <= totalPages; i++ {
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...) // 배열의 콘텐츠를 모아 다시 배열로 묶음 -> [] + [] + [] + [] = []
 	}
+	fmt.Println(jobs)
+}
 
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
 
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
+	defer res.Body.Close()
 
-	for url, status := range results {
-		fmt.Println(url, status)
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".item_recruit").Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	//detailURL := baseDetailURL + "&rec_idx=" + id
+	id, _ := card.Attr("value")
+	badge := cleanString(card.Find(".area_badge>span").Text())
+	title := cleanString(card.Find(".job_tit>a").Text())
+	corporation := cleanString(card.Find(".area_corp>strong").Text())
+	return extractedJob{id, badge, title, corporation}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func getPages() int {
+	pages := 0
+
+	res, err := http.Get(baseURL)
+
+	checkErr(err)
+	checkCode(res)
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+		//fmt.Println(pages)
+	})
+
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
 
-// c chan<- : 메시지를 보낼 수만 있다 선언
-func hitURL(url string, c chan<- resultRequest) {
-	resp, err := http.Get(url)
-	status := "OK"
-	if err != nil || resp.StatusCode >= 400 {
-		status = "FAILED"
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status : ", res.StatusCode)
 	}
-	c <- resultRequest{url: url, status: status}
 }
